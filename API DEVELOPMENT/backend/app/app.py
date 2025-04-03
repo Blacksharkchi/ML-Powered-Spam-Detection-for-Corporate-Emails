40e4b042a08f268c34b991086f4d4985acc1ae22
Daniel Giathi <blacksharkchi@proton.me>
2025-04-03 10:13:17 +0300

    update model to use svm

--- API DEVELOPMENT/backend/app/app.py
+++ API DEVELOPMENT/backend/app/app.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.INFO)
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
